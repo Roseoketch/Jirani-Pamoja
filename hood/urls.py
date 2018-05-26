aee57d8c8d6c64e8af15886745b2b25cdebfedd0
Roseoketch <oketchrose@gmail.com>
2018-05-26 20:23:27 +0300

    new_post function is added

--- hood/urls.py
+++ hood/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url(r'^new/',views.create_profile,name='new_profile'),
+    url(r'^post/',views.new_post,name='post'),
+
 
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
